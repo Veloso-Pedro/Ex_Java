c35ca46434f04dd00e1ff6e603994d44c2d43518
Veloso-Pedro <pevesalima@Gmail.com>
2025-03-19 11:29:32 -0300

    upd & add 19/03

--- Roleta.java
+++ Roleta.java
@@ -17,9 +17,27 @@ public class Roleta {
             System.out.println("Selecione um número de 0 á 10"); int esc = scanner.nextInt();
 
             if (sorte == esc){
+                System.out.println("=============================");
                 System.out.println("Você Ganhouu!!!");
+                System.out.println("O número da sorte era: " + sorte);
+                System.out.println("=============================");
             }else{
+                System.out.println("=============================");
                 System.out.println("Você perdeu!!!");
+                System.out.println("O número da sorte era: " + sorte);
+                System.out.println("=============================");
+            }
+
+            System.out.println("Quer jogar denovo?");
+            System.out.println("1 - sim");
+            System.out.println("2 - não");
+            int esc2 = scanner.nextInt();
+
+            if (esc2 == 2){
+                System.out.println("=============================");
+                System.out.println("OBRIGADO POR JOGAR!!!");
+                System.out.println("=============================");
+                break; 
             }
         }
     }
--- perimetrocirc.java
+++ perimetrocirc.java
@@ -4,20 +4,41 @@ public class perimetrocirc {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Calculadora de Perímetro da Circunferencia\n");
-        System.out.print("-------------------------------------------\n");
-        System.out.print("Você deseja calcular o perímetro atraves do raio ou do diametro?\n");
-        System.out.print("1 - Raio\n");
-        System.out.print("2 - Diametro\n");
-        int esc = scanner.nextInt();
+        while (true){
 
-        if (esc == 1){
-            System.out.print("Digite o raio: "); double raio = scanner.nextDouble();
-            System.out.printf("O perímetro da circunferencia é %.2f %n", (2*Math.PI*raio));
-        } else if(esc == 2){
-            System.out.print("Digite seu diametro: "); double diametro = scanner.nextDouble();
-            System.out.printf("O perímetro da circunferencia é %.2f %n",(diametro*Math.PI));
+            System.out.print("Calculadora de Perímetro da Circunferencia\n");
+            System.out.print("-------------------------------------------\n");
+            System.out.print("Você deseja calcular o perímetro atraves do raio ou do diametro?\n");
+            System.out.print("1 - Raio\n");
+            System.out.print("2 - Diametro\n");
+            System.out.println("3 - Sair");
+            System.out.println("=========================================");
+            int esc = scanner.nextInt();
+
+            if (esc == 1){
+                System.out.println("=========================================");
+                System.out.print("Digite o raio: "); double raio = scanner.nextDouble();
+                System.out.printf("O perímetro da circunferencia é %.2f %n", (2*Math.PI*raio));
+                System.out.println("=========================================");
+            } else if(esc == 2){
+                System.out.println("=========================================");
+                System.out.print("Digite seu diametro: "); double diametro = scanner.nextDouble();
+                System.out.printf("O perímetro da circunferencia é %.2f %n",(diametro*Math.PI));
+                System.out.println("=========================================");
+            }else if(esc == 3){
+                System.out.println("=========================================");
+                System.out.println("OBRIGADO POR TESTAR!!");
+                System.out.println("Saindo...");
+                System.out.println("=========================================");
+                break;
+            }else{
+                System.out.println("=========================================");
+                System.out.println("ERRO!!");
+                System.out.println("DIGITE UM VALOR VALIDO");
+                System.out.println("=========================================");
+            }
         }
-    scanner.close();
+        scanner.close();
     }
 }
+
