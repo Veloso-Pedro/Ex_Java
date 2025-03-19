372ce251c2016580047172241ccbd8a08f270156
Veloso-Pedro <pevesalima@Gmail.com>
2025-03-19 11:00:17 -0300

    att & add 19/03
    Concertando alguns erros nas linhas 23,25 e 36

--- Calculadora_SomaeSub.java
+++ Calculadora_SomaeSub.java
@@ -15,13 +15,14 @@ public class Calculadora_SomaeSub{
 
             if (Escolha == 3){
                 System.out.println("Saindo...");
+                System.out.println("OBRIGADO POR TESTAR!!!");
                 break;
             }
             if (Escolha == 1 || Escolha == 2){
                 System.out.println("Digite o primeiro número: ");
-                double num1 = scanner.nextInt();
+                double num1 = scanner.nextDouble();
                 System.out.println("DIgite o segundo númeor: ");
-                double num2 = scanner.nextInt();
+                double num2 = scanner.nextDouble();
                 
                 if (Escolha == 1){
                     System.out.printf("O resultado da soma é: %.2f %n ", (num1 + num2));    
@@ -31,8 +32,7 @@ public class Calculadora_SomaeSub{
             } else{
                 System.out.println("ERRO!! escolha um operador valido!");
             } 
-            scanner.close();
-       
         }
+        scanner.close();
     }
 }
