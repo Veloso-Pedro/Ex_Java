61127e106c955e780ba4b9511fe96c660c5469d7
Veloso-Pedro <pevesalima@Gmail.com>
2025-04-14 19:36:04 -0300

    upd & add 14/04

--- Fibonacci.java
+++ Fibonacci.java
@@ -19,5 +19,6 @@ public class Fibonacci {
                 System.out.println(F);
             }
         }
+        scn.close();
     }
 }
---
+++ MediaeFrequencia.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MediaeFrequencia {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Infome a média necessária para aprovação:  ");
+        double  mediaN = scn.nextDouble();
+        System.out.println("Digite a a freuencia mínima necessário:  ");
+        double freqN = scn.nextDouble();
+        System.out.println("============================================");
+        System.out.println("Digite sua média: ");
+        double media = scn.nextDouble();
+        System.out.println("Digite sua frequencia: ");
+        double freq = scn.nextDouble();
+
+        if (mediaN == media && freqN == freq){
+            System.out.println("Aluno Aprovado!!!");
+        }else{
+            System.out.println("Reprovado!!");
+        }
+    }
+}
